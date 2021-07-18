2fd1c99f0ab40172d7c4740be8336606b68a835c
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2021-07-18 11:49:19 +0900

    記事編集・削除モーダルの表示

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\ArticleRequest;
 
 class ArticleController extends Controller
 {
+    //記事一覧表示
     public function index()
     {
         $articles = Article::all()->sortByDesc('created_at');
@@ -15,16 +16,24 @@ class ArticleController extends Controller
         return view('articles.index', ['articles' => $articles]);
     }
 
+    //記事作成表示
     public function create()
     {
         return view('articles.create');
     }
 
+    //記事作成処理
     public function store(ArticleRequest $request, Article $article)
     {
         $article->fill($request->all());
         $article->user_id = $request->user()->id;
         $article->save();
         return redirect()->route('articles.index');
+    }
+
+    //記事編集表示
+    public function edit(Article $article)
+    {
+        return view('articles..edit',['article' => $article]);
     }
 }
