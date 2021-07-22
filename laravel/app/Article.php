38dde70425fe773d6ac725e4d9a39b5df33f1d47
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2021-07-22 09:46:50 +0900

    ユーザーが記事をいいね済みか判定する

--- laravel/app/Article.php
+++ laravel/app/Article.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
@@ -12,9 +13,23 @@ class Article extends Model
         'body',
     ];
 
-    public function user():BelongsTo //戻り値の型をBelongsToに指定
+    public function user(): BelongsTo //戻り値の型をBelongsToに指定
     {
         //articleとuserは多対1
         return $this->belongsTo('App\User');
+    }
+
+    public function likes(): BelongsToMany
+    {
+        //articleとlikesは多対多
+        return $this->belongsToMany('App\User', 'likes')->withTimestamps();
+    }
+
+    public function isLikedBy(?User $user): bool
+    {
+        //likesテーブルに$userのidが存在する場合はtrue,存在しなければfalse
+        return $user
+            ? (bool) $this->likes->where('id', $user->id)->count()
+            : false;
     }
 }
