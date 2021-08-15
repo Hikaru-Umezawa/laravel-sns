35673e150e941449145aa5d65837568d8a1e647e
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2021-08-15 15:59:03 +0900

    fix: タイポ

--- laravel/app/User.php
+++ laravel/app/User.php
@@ -47,12 +47,12 @@ class User extends Authenticatable
 
     public function followers(): BelongsToMany
     {
-        return $this->belomgsToMany('App\User', 'follows', 'followee_id', 'follower_id')->withTimestamps();
+        return $this->belongsToMany('App\User', 'follows', 'followee_id', 'follower_id')->withTimestamps();
     }
 
     public function followings(): BelongsToMany
     {
-        return $this->belomgsToMany('App\User', 'follows', 'follower_id', 'followee_id')->withTimestamps();
+        return $this->belongsToMany('App\User', 'follows', 'follower_id', 'followee_id')->withTimestamps();
     }
 
     public function isFollowedBy(?User $user): bool
